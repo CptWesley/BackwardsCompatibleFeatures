822d1e8129610a652051f96a3b897fc2123aac5b
Corniel Nobel <mail@corniel.nl>
2023-06-09 21:38:29 +0200

    The pure attribute did not exist until .NET-framework 4.0

---
+++ src/BackwardsCompatibleFeatures/PureAttribute.cs
@@ -0,0 +1,14 @@
+﻿#if !NET40_OR_GREATER && !NETSTANDARD1_0_OR_GREATER && !NETCOREAPP1_0_OR_GREATER
+
+namespace System.Diagnostics.Contracts;
+
+/// <summary>
+/// Indicates that a type or method is pure, that is, it does not make any visible
+/// state changes.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.Parameter | AttributeTargets.Delegate, AllowMultiple = false, Inherited = true)]
+[Conditional("CONTRACTS_FULL")]
+public sealed class PureAttribute : Attribute
+{
+}
+#endif
